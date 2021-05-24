a626e7b4ba33284b1357792887f34b3fcf05658c
ciwchris <350318+ciwchris@users.noreply.github.com>
2021-05-23 22:28:26 -0700

    Post on absent mindedness and automatic processing

---
+++ posts/examples.ex
@@ -0,0 +1,27 @@
+
+``` text/2-3
+// this is a command
+function myCommand() {
+	let counter = 0;
+	counter++;
+}
+
+// Test with a line break above this line.
+console.log('Test');
+```
+
+<a href="{{ '/posts/firstpost/' | url }}">First post</a>
+
+
+``` js/2/4
+// this is a command
+function myCommand() {
+	let counter = 0;
+
+	counter++;
+
+}
+
+// Test with a line break above this line.
+console.log('Test');
+```
